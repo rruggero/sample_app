29b1bd865457d2215c5fde6e01c899c1d77967f5
Ryan Ruggero <rruggero@gmail.com>
2013-09-10 19:19:53 -0400

    Finish layout and routes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,41 +1,57 @@
 require 'spec_helper'
 
 describe "StaticPages" do
+  
+  let(:base_title) {"Ruby on Rails Tutorial Sample App"}
+  
   describe "Home page" do
     it "should have the content 'Sample App'" do
-      visit '/static_pages/home'
+      visit root_path
       expect(page).to have_content('Sample App')
     end
     
     it "should have the title 'Home'" do
-      visit '/static_pages/home'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
+      visit root_path
+      expect(page).to have_title("#{base_title} | Home")
     end
   end
   
   describe "Help page" do
     
     it "should have the content 'Help'" do
-      visit '/static_pages/help'
+      visit help_path
       expect(page).to have_content('Help')
     end
     
      it "should have the title 'Help'" do
-        visit '/static_pages/help'
-        expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
+        visit help_path
+        expect(page).to have_title("#{base_title} | Help")
       end
   end
   
   describe "About page" do
     
     it "should have the content 'About Us'" do
-      visit '/static_pages/about'
+      visit about_path
       expect(page).to have_content('About Us')
     end
     
      it "should have the title 'Abput Us'" do
-        visit '/static_pages/about'
-        expect(page).to have_title("Ruby on Rails Tutorial Sample App | About Us")
+        visit about_path
+        expect(page).to have_title("#{base_title}  | About Us")
+      end
+  end
+  
+  describe "Contact page" do
+    
+    it "should have the content 'Contact'" do
+      visit contact_path
+      expect(page).to have_content('Contact')
+    end
+    
+     it "should have the title 'Contact'" do
+        visit contact_path
+        expect(page).to have_title("#{base_title} | Contact")
       end
   end
 end
